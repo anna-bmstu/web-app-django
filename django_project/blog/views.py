171bce018294adec6e769016965abc59f43769a4
anna <Ane4kaGr@mail.ru>
2022-07-25 14:56:17 +0300

    created models

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Anna',
-        'title': 'Blog Post 1',
-        'content': 'My first post',
-        'date_posted': 'July 21, 2022'
-    },
-    {
-        'author': 'Elon Musk',
-        'title': 'Blog Post 2',
-        'content': 'Second post',
-        'date_posted': 'July 18, 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
